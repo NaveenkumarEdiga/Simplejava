f9065ece0c5f83d58111da358eb577dce17ca443
NaveenkumarEdiga <98840433+NaveenkumarEdiga@users.noreply.github.com>
2022-02-02 06:27:39 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,25 @@
+ class hello
+ {
+ 	public static void main(String[] args)
+	{
+		System.out.println("hello java");
+		m1();
+		System.out.println("thanks for  providing free software");
+		System.out.println(" to learn java require almost one year or two years");
+		b.m1();
+	 }
+	public static void m1()
+	{
+		System.out.println("hello java");
+		System.out.println("thanks for  providing free software");
+		m2();
+		System.out.println(" to learn java require almost one year or two years");
+	 }
+	public static void m2()
+	{
+		System.out.println("hello java");
+		System.out.println("thanks for  providing free software");
+		System.out.println(" to learn java require almost one year or two years");
+                
+	 }
+}
